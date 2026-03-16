f325f8a0e56b67c4f194c0e6979c5b5a781fdfc1
Vasu margana <aveenashvasu2024@gmail.com>
2026-03-16 21:03:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,9 +51,6 @@ model_name = st.sidebar.selectbox(
     "Model",
     [
         "llama-3.1-8b-instant",
-        "llama-3.1-70b-versatile",
-        "mixtral-8x7b-32768",
-        "gemma2-9b-it"
     ]
 )
 
